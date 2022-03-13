b07d66e9f0e1706dfd4c794e37f6bdf0643861ff
zhao365845726@163.com <zhao365845726@163.com>
2022-03-14 06:04:07 +0800

    增加单元测试

---
+++ src/CSRedisCoreTests/CSRedisClientTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CSRedis;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSRedis.Tests
+{
+    [TestClass()]
+    public class CSRedisClientTests : TestBase
+    {
+        [TestMethod()]
+        public void SetTest()
+        {
+            rds.Set("2022:martyzane:language", ".net");
+            string result = rds.Get("2022:martyzane:language");
+            Assert.AreEqual(".net", result);
+
+            rds.Set("2022:martyzane:demo", 1,60);
+            rds.SetNx("2022:martyzane:demo-a", 2);
+        }
+    }
+}
